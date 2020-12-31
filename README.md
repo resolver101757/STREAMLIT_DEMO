a600b2dfc2a6f9c6759b38685cbce805831185a1
resolver101757 <kelster101757@hotmail.com>
2020-12-31 15:47:19 +0000

    updated readme, merged facedetection to streamlit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # STREAMLIT_DEMO
+
+Some scripts testing out the streamlit app
+
+First_app.py shows a bunch of objects thrown onto the website 
+take_a_picture.py draws boundries around a still photo teken on the webcam (or what ever is the default camera)
+open_cv_face_detection _video.py is simular take_a_picture.py but shows it in a video 
+
+
+haarcascades folder has different opencv face detection parameters 
+
+The openCV was taken from this article : https://towardsdatascience.com/computer-vision-for-beginners-part-3-79de62dbeef7
---
+++ open_cv_face_detection _video.py
@@ -0,0 +1,38 @@
+# this shows the video in streamlit 
+# 
+
+from cv2 import *
+import numpy as np
+import streamlit as st
+import time
+
+
+# Define detect function
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+
+# Detects faces when passed a image and draws a box around the returned face in the image
+def detect_face(img):
+    img_copy = img.copy()
+    face_rects = face_cascade.detectMultiScale(img_copy ) 
+    for (x, y, w, h) in face_rects:
+        cv2.rectangle(img_copy, (x, y), (x+w, y+h), (255, 255, 255), 3)
+        
+    return img_copy
+
+# Call the cam
+cap = cv2.VideoCapture(0) 
+
+# ST.button shows the start button
+# When pressed it shows a video of the start button
+if st.button('Start'):
+
+    cap.set(cv2.CAP_PROP_FPS, 25) # sets frames per second. 
+    image_placeholder = st.empty() # 
+
+    while True: 
+        # reads the image and stores in image       
+        success, image = cap.read()
+        if not success: 
+            break
+        image = detect_face(image) #detects faces and draws a box around the face
+        image_placeholder.image(image, channels="BGR") #displays the video of the image
